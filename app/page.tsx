2b4fd797a053cd2186bde7627411891f067edb6a
teneieiza <tenmonsterten@gmail.com>
2025-10-17 15:53:43 +0700

    Edit app page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center items-center h-full">
       <h1>Test</h1>
     </div>
   );

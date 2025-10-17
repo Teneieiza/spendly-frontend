f47db39c8ba599ab3a8dd5b9e9e1d0d692cd6e21
teneieiza <tenmonsterten@gmail.com>
2025-10-17 17:34:05 +0700

    Edit layout for Sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,14 +18,14 @@ export const metadata: Metadata = {
 };
 
 import Navbar from "@/components/layout/Navbar"
-// import Sidebar from "@/components/layout/Sidebar"
+import Sidebar from "@/components/layout/Sidebar"
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen bg-white flex justify-center`}>
         <div className="w-full max-h-screen m-5 bg-white border border-gray-200 shadow-sm rounded-2xl flex overflow-hidden">
-          {/* <Sidebar /> */}
+          <Sidebar />
           <div className="flex-1 flex flex-col">
             <Navbar />
             <main className="p-6 bg-[#f9fafb] flex-1 overflow-y-auto">{children}</main>

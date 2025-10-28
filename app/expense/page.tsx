1969d8dd3a89e2df00161f13d8542555ab032edd
teneieiza <tenmonsterten@gmail.com>
2025-10-28 16:38:45 +0700

    Edit expense page

--- app/expense/page.tsx
+++ app/expense/page.tsx
@@ -1,7 +1,16 @@
-export default function Expense() {
+"use client";
+
+import WeekHeader from "@/components/calendar/WeekHeader";
+import TimeGrid from "@/components/calendar/TimeGrid";
+
+export default function ExpensePage() {
   return (
-    <div className="flex justify-center items-center h-full">
-      <h1>Expense</h1>
+    <div className="flex flex-col h-full overflow-y-auto">
+      <div className="sticky top-0 z-10 bg-white">
+        <WeekHeader />
+      </div>
+
+      <TimeGrid />
     </div>
   );
 }

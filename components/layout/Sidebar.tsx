1631c0963c9ed7625a635ff0eb346945c5100f5c
teneieiza <tenmonsterten@gmail.com>
2025-10-28 16:40:20 +0700

    Edit sidebar for path expense

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -1,43 +1,55 @@
-// components/sidebar.tsx
-"use client";
+'use client'
 
-import { useState } from "react";
-import Link from "next/link";
-import { BarChart, Wallet, Settings, ChevronDown, Menu, BadgeDollarSign, ChartColumn, LogOut, Calendar } from "lucide-react";
-import { cn } from "@/lib/utils";
-import { usePathname } from "next/navigation";
+import { useState } from 'react'
+import Link from 'next/link'
+import {
+  Wallet,
+  Settings,
+  ChevronDown,
+  Menu,
+  BadgeDollarSign,
+  ChartColumn,
+  LogOut,
+} from 'lucide-react'
+import { cn } from '@/lib/utils'
+import { usePathname } from 'next/navigation'
+import { useCalendarStore } from "@/store/useCalendarStore";
+import { Calendar } from "@/components/ui/calendar";
+import { startOfWeek, endOfWeek } from "date-fns";
 
 export default function Sidebar() {
-  const pathname = usePathname();
-  const [open, setOpen] = useState(true);
-  const [showGeneral, setShowGeneral] = useState(true);
-  const [showSetting, setShowSetting] = useState(true);
+  const pathname = usePathname()
+  const [open, setOpen] = useState(true)
+  const [showGeneral, setShowGeneral] = useState(true)
+  const [showSetting, setShowSetting] = useState(true)
+
+  const currentDate = useCalendarStore((s) => s.currentDate)
+  const setState = useCalendarStore.setState
 
   return (
     <div
       className={cn(
-        "h-full bg-white shadow-md transition-all border-r border-gray-200 z-50",
-        open ? "w-64" : "w-22"
+        'z-50 h-full border-r border-gray-200 bg-white shadow-md transition-all',
+        open ? 'w-64' : 'w-22',
       )}
     >
-      <div className="flex flex-col justify-between h-full">
+      <div className="flex h-full flex-col justify-between">
         {/* Header */}
         <div>
-          <div className="flex items-center justify-between p-4 border-b h-18">
+          <div className="flex h-18 items-center justify-between border-b p-4">
             <div className="flex items-center gap-2">
-              <BadgeDollarSign size={24}/>
-              {open && <span className="font-semibold text-lg">Spendly</span>}
+              <BadgeDollarSign size={24} />
+              {open && <span className="text-lg font-semibold">Spendly</span>}
             </div>
             <button onClick={() => setOpen(!open)} className="text-gray-600">
               <Menu size={20} />
             </button>
           </div>
 
-
           {/* General Section */}
           <div className="mt-4">
             <button
-              className="flex items-center justify-between w-full pl-3 pr-4 py-2 text-sm font-semibold text-gray-600"
+              className="flex w-full items-center justify-between py-2 pr-4 pl-3 text-sm font-semibold text-gray-600"
               onClick={() => setShowGeneral(!showGeneral)}
             >
               <div className="flex items-center gap-2 text-gray-400">
@@ -45,7 +57,10 @@ export default function Sidebar() {
               </div>
               {open && (
                 <ChevronDown
-                  className={cn("transition-transform", showGeneral ? "rotate-180" : "")}
+                  className={cn(
+                    'transition-transform',
+                    showGeneral ? 'rotate-180' : '',
+                  )}
                   size={18}
                 />
               )}
@@ -53,45 +68,17 @@ export default function Sidebar() {
 
             {showGeneral && (
               <div className="ml-6 flex flex-col gap-1 font-semibold">
-                <Link
-                  href="/dashboard"
-                  className={cn(
-                    "flex items-center gap-2 px-3 py-2 text-sm rounded-md hover:bg-gray-100 transition-colors",
-                    open ? "w-[90%]" : "w-[70%]",
-                    pathname === "/dashboard" ? "bg-gray-200 font-bold" : "text-gray-700"
-                  )}
-                >
-                  {open ? <span className="flex gap-2"><ChartColumn size={18} /> Dashboard</span>: <ChartColumn size={18} />}
-                </Link>
-                <Link
-                  href="/expense"
-                  className={cn(
-                    "flex items-center gap-2 px-3 py-2 text-sm rounded-md hover:bg-gray-100 transition-colors",
-                    open ? "w-[90%]" : "w-[70%]",
-                    pathname === "/expense" ? "bg-gray-200 font-bold" : "text-gray-700"
-                  )}
-                >
-                  {open ? <span className="flex gap-2"> <Wallet size={18}/> Expense</span>: <Wallet size={18}/>}
-                </Link>
-                <Link
-                  href="/overall"
-                  className={cn(
-                    "flex items-center gap-2 px-3 py-2 text-sm rounded-md hover:bg-gray-100 transition-colors",
-                    open ? "w-[90%]" : "w-[70%]",
-                    pathname === "/overall" ? "bg-gray-200 font-bold" : "text-gray-700"
-                  )}
-                >
-                  {open ? <span className="flex gap-2"> <Calendar size={18}/> Overall</span>: <Wallet size={18}/>}
-                </Link>
+                <SidebarLink open={open} pathname={pathname} href="/dashboard" icon={<ChartColumn size={18} />} label="Dashboard" />
+                <SidebarLink open={open} pathname={pathname} href="/expense" icon={<Wallet size={18} />} label="Expense" />
+                <SidebarLink open={open} pathname={pathname} href="/overall" icon={<Wallet size={18} />} label="Overall" />
               </div>
             )}
           </div>
 
-
           {/* Setting Section */}
           <div className="mt-4">
             <button
-              className="flex items-center justify-between w-full pl-3 pr-4 py-2 text-sm font-semibold text-gray-600"
+              className="flex w-full items-center justify-between py-2 pr-4 pl-3 text-sm font-semibold text-gray-600"
               onClick={() => setShowSetting(!showSetting)}
             >
               <div className="flex items-center gap-2 text-gray-400">
@@ -99,36 +86,71 @@ export default function Sidebar() {
               </div>
               {open && (
                 <ChevronDown
-                  className={cn("transition-transform", showSetting ? "rotate-180" : "")}
+                  className={cn(
+                    'transition-transform',
+                    showSetting ? 'rotate-180' : '',
+                  )}
                   size={18}
                 />
               )}
             </button>
 
-            {showSetting &&(
-              <div className="ml-6 mt-1 space-y-1 font-semibold">
-                <Link
-                  href="/setting"
-                    className={cn(
-                    "flex items-center gap-2 px-3 py-2 text-sm rounded-md hover:bg-gray-100 transition-colors",
-                    open ? "w-[90%]" : "w-[70%]",
-                    pathname === "/setting" ? "bg-gray-200 font-bold" : "text-gray-700"
-                  )}
-                >
-                  {open ? <span className="flex gap-2"> <Settings size={18}/> Setting</span>: <Settings size={18}/>}
-                </Link>
+            {showSetting && (
+              <div className="mt-1 ml-6 space-y-1 font-semibold">
+                <SidebarLink open={open} pathname={pathname} href="/setting" icon={<Settings size={18} />} label="Setting" />
               </div>
             )}
           </div>
+
+        {/* Calendar เฉพาะหน้า Expense */}
+        {open && pathname === '/expense' && (
+          <div className="border-t mt-8">
+            <Calendar
+              mode="single"
+              selected={currentDate}
+              onSelect={(date) => {
+                if (!date) return
+                setState({
+                  currentDate: date,
+                  weekStart: startOfWeek(date, { weekStartsOn: 0 }),
+                  weekEnd: endOfWeek(date, { weekStartsOn: 0 }),
+                })
+              }}
+            />
+          </div>
+        )}
+
         </div>
 
+
+
         {/* Logout */}
-        <div className="px-4 mb-4 font-bold">
-          <button className="w-full flex justify-center items-center bg-gray-100 py-2 text-sm rounded-md hover:bg-gray-200 transition-colors drop-shadow-lg">
-            {open ? "LOGOUT" : <LogOut size={16} />}
+        <div className="mb-4 px-4 font-bold">
+          <button className="flex w-full items-center justify-center rounded-md bg-gray-100 py-2 text-sm drop-shadow-lg transition-colors hover:bg-gray-200">
+            {open ? 'LOGOUT' : <LogOut size={16} />}
           </button>
         </div>
       </div>
     </div>
-  );
+  )
+}
+
+// ✅ แยก component ย่อยให้สะอาดขึ้น
+function SidebarLink({ href, icon, label, pathname, open }: any) {
+  return (
+    <Link
+      href={href}
+      className={cn(
+        'flex items-center gap-2 rounded-md px-3 py-2 text-sm transition-colors hover:bg-gray-100',
+        open ? 'w-[90%]' : 'w-[70%]',
+        pathname === href ? 'bg-gray-200 font-bold' : 'text-gray-700',
+      )}
+    >
+      {open ? (
+        <span className="flex gap-2">{icon} {label}</span>
+      ) : (
+        icon
+      )}
+    </Link>
+  )
 }

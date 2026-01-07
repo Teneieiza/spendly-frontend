bc14996ef34f0531027bd76d791c700917a8a889
teneieiza <tenmonsterten@gmail.com>
2026-01-07 18:15:55 +0700

    Change auth path

--- app/(main)/dashboard/page.tsx
+++ app/(main)/dashboard/page.tsx
@@ -1,4 +1,4 @@
-import { auth } from '@/auth'
+import { auth } from '@/lib/auth'
 import DashboardClient from './dashboard.client'
 
 export default async function DashboardPage() {
--- middlewares/auth.middleware.ts
+++ middlewares/auth.middleware.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import { auth } from '@/auth'
+import { auth } from '@/lib/auth'
 
 const PROTECTED_ROUTES = ['/dashboard', '/expense', '/overall', '/setting']
 
